d36d025f6473f69424d2594356a7131add1eae55
Jaydeep Parmar <jaydeepparmarjp89@gmail.com>
2023-10-13 17:04:30 +0530

    Updates

---
+++ precache-manifest.9647fc20fa669e4efd008d618f87ccc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de4b08d653cefde187cc297aa914b6c",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "af6f403773aeef77aa3d",
+    "url": "/textutils/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "bf5c34c705ad50e3c406",
+    "url": "/textutils/static/js/2.af3fc02c.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/textutils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "af6f403773aeef77aa3d",
+    "url": "/textutils/static/js/main.68b0ffb6.chunk.js"
+  },
+  {
+    "revision": "9eff2318fe253be3dc05",
+    "url": "/textutils/static/js/runtime~main.43961880.js"
+  }
+]);
